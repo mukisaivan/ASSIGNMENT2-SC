f8631c62de6a39c0707e658811a680af378372aa
Mukisa Ivan <106663129+mukisaivan@users.noreply.github.com>
2023-03-30 18:15:40 +0300

    commenting in main

--- Main.java
+++ Main.java
@@ -1,29 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Circle circle = new Circle();
-        // circle.perimeter();
-        // circle.area();
-        // circle.position();
-        // circle.paint(null);  //drawshape
-        
-
-        
         // Rectangle rectangle = new Rectangle();
         // rectangle.perimeter();
         // rectangle.area();
         // rectangle.drawShape();
         // rectangle.position();
 
-
-        Square square = new Square();
+        // Square square = new Square();
         // square.perimeter();
         // square.area();
-        square.position();
+        // square.position();
         // square.drawShape();
         
-
-        
-        
+        // Circle circle = new Circle();
+        // circle.perimeter();
+        // circle.area();
+        // circle.position();
+        // circle.paint(null);  //drawshape
+         
     }
 }
