7ae3b48f956660b93e8987584ed29dfd21b1872b
Mukisa Ivan <106663129+mukisaivan@users.noreply.github.com>
2023-03-30 18:19:21 +0300

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -13,8 +13,8 @@ public class Circle extends JFrame{
 
     final double PI = 3.14;
 
+    
     // Circle(int radius, int centreX, int centreY) {
-
     //     this.radius = Integer.parseInt(JOptionPane.showInputDialog("Enter circle radius: "));
     //     this.centreX = centreX;
     //     this.centreY = centreY;
@@ -22,64 +22,38 @@ public class Circle extends JFrame{
     
 
     public void perimeter() {
-
         this.radius = Integer.parseInt(JOptionPane.showInputDialog("Enter circle radius: "));
         double result = 2 * PI * this.radius;
 
         System.out.print("Perimeter: " + result + "\n");
         JOptionPane.showMessageDialog(null, "The perimeter of the circle is " + result + " UNITS");
-        System.out.println();
-
-        
-
+        System.out.println(); 
     }
     
-    public void area() {
-        
+    public void area() { 
         this.radius = Integer.parseInt(JOptionPane.showInputDialog("Enter circle radius: "));  
         double result = PI * Math.pow(this.radius, 2);
         JOptionPane.showMessageDialog(null, "The area of the ciircle is " + result + " SQUARE UNITS");
-
         System.out.println("Area: "+ result + "\n" );
-        
     }
     
-    // public void position() {
-    //     this.centreX = Integer.parseInt(JOptionPane.showInputDialog("Enter centre X cordinate: "));
-    //     this.centreY = Integer.parseInt(JOptionPane.showInputDialog("Enter centre Y cordinate: "));
-    //     JOptionPane.showMessageDialog(null, "The position of the circle is " + "(" + this.centreX + "," + this.centreY + ")");
-
-    //     if (centreX == 0 || centreY == 0) {
-    //         System.out.println("you have specified the centre");
-    //     } else {
-    //         System.out.println("Position (" + this.centreX + ", " + this.centreY + ")");
-    //     }
-
-    // }
-   
-
-
     
     @Override
     public void paint(Graphics g) {
-
         this.setSize(750, 750);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-
+        
         Graphics2D g2d = (Graphics2D) g;
         g2d.setPaint(Color.red);
 
+        //setters
         this.radius = Integer.parseInt(JOptionPane.showInputDialog("Enter circle radius: "));
         this.centreX = Integer.parseInt(JOptionPane.showInputDialog("Enter centre X cordinate: "));
         this.centreY = Integer.parseInt(JOptionPane.showInputDialog("Enter centre Y cordinate: "));
 
         g2d.fillOval(this.centreX, this.centreY, this.radius, this.radius);
-        
-
-
         
     }
 
